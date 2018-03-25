01c73e2498658e95175f5c1c37c45a0bcd603eac
Jonathan <jleack@gmail.com>
2018-03-24 19:35:54 -0700

    Updated colors

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,6 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 
-/*======================================================================
-// 
-======================================================================*/
 const layout = (props) => (
   <Auxiliary>
     <div></div>
